afb9a4f56ccb13f9f258e96ae6ad093c21f51da7
TECHRISTO <49098166+techristo@users.noreply.github.com>
2021-09-19 06:40:14 +0300

    Initial commit

---
+++ App.svelte
@@ -0,0 +1,16 @@
+<script>
+	import Button from "./Button.svelte";
+</script>
+
+<style>
+  main {
+    font-family: sans-serif;
+    text-align: center;
+  }
+</style>
+
+<main>
+	<h1>Take Tokens</h1>
+	<h2>Start clicking the button to take free tokens</h2>
+	<Button />
+</main>
---
+++ Button.svelte
@@ -0,0 +1,21 @@
+<script>
+	let count = 0;
+
+	function handleClick() {
+	  count += 1;
+	}
+</script>
+
+<style>
+	button {
+	  background: #ff3e00;
+	  color: white;
+	  border: none;
+	  padding: 8px 12px;
+	  border-radius: 10px;
+	}
+</style>
+
+<button on:click={handleClick}>
+   {count} {count === 1 ? 'token' : 'tokens'}
+</button>
